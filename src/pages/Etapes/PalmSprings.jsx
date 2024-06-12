34b94ee5f202f45b71f303bfe312117176f68aea
Cédric Bison <cedric.bison@gmail.com>
2024-06-12 07:27:32 +0200

    chore: Update styles and components for better performance and user experience

--- src/pages/Etapes/PalmSprings.jsx
+++ src/pages/Etapes/PalmSprings.jsx
@@ -13,6 +13,35 @@ import westernFenceLizard from '../../assets/images/palm-springs/7.jpg';
 import indianPaintbrush from '../../assets/images/palm-springs/8.jpg';
 import sarcodesSanguinea from '../../assets/images/palm-springs/9.jpg';
 import tronc from '../../assets/images/palm-springs/10.jpg';
+import jt1 from '../../assets/images/palm-springs/11.jpg';
+import jt2 from '../../assets/images/palm-springs/12.jpg';
+import jt3 from '../../assets/images/palm-springs/13.jpg';
+import jt4 from '../../assets/images/palm-springs/14.jpg';
+import jt5 from '../../assets/images/palm-springs/15.jpg';
+import jt6 from '../../assets/images/palm-springs/16.jpg';
+import jt7 from '../../assets/images/palm-springs/17.jpg';
+import jt8 from '../../assets/images/palm-springs/18.jpg';
+import jt9 from '../../assets/images/palm-springs/19.jpg';
+import jt10 from '../../assets/images/palm-springs/20.jpg';
+import jt11 from '../../assets/images/palm-springs/21.jpg';
+import jt12 from '../../assets/images/palm-springs/22.jpg';
+import jt13 from '../../assets/images/palm-springs/23.jpg';
+import jt14 from '../../assets/images/palm-springs/24.jpg';
+import jt15 from '../../assets/images/palm-springs/25.jpg';
+import jt16 from '../../assets/images/palm-springs/26.jpg';
+import jt17 from '../../assets/images/palm-springs/27.jpg';
+import jt18 from '../../assets/images/palm-springs/28.jpg';
+import jt19 from '../../assets/images/palm-springs/29.jpg';
+import jt20 from '../../assets/images/palm-springs/30.jpg';
+import jt21 from '../../assets/images/palm-springs/31.jpg';
+import jt22 from '../../assets/images/palm-springs/32.jpg';
+import jt23 from '../../assets/images/palm-springs/33.jpg';
+import jt24 from '../../assets/images/palm-springs/34.jpg';
+import jt25 from '../../assets/images/palm-springs/35.jpg';
+import jt26 from '../../assets/images/palm-springs/36.jpg';
+import jt27 from '../../assets/images/palm-springs/37.jpg';
+import jt28 from '../../assets/images/palm-springs/38.jpg';
+import jt29 from '../../assets/images/palm-springs/39.jpg';
 
 export default function PalmSprings() {
 	return (
@@ -58,33 +87,73 @@ export default function PalmSprings() {
 				</PhotosContainer>
 				<h2>La faune et la flore</h2>
 				<p>
-					La faune et la flore de Palm Springs sont riches et variées.
-					On peut y observer de nombreux oiseaux, comme le Geai de
-					Steller ou encore le Lézard à clôture de l&apos;Ouest. Côté
-					flore, on peut admirer des fleurs comme l&apos;Indian
-					Paintbrush, la Sarcodes sanguinea, ou encore des arbres
-					majestueux.
+					La faune et la flore y sont riches et variées. On peut y
+					observer de nombreux oiseaux, comme le Geai de Steller ou
+					encore le Lézard à clôture de l&apos;Ouest. Côté flore, on
+					peut admirer des fleurs comme l&apos;Indian Paintbrush, la
+					Sarcodes sanguinea, ou encore des arbres majestueux.
 				</p>
 				<PhotosContainer className='five-pictures variant'>
 					<Photo src={stellerJay} alt='Geai de Steller' />
 					<Photo
 						src={westernFenceLizard}
-						alt='Lézard à clôture de l Ouest'
+						alt="Lézard à clôture de l'Ouest"
 					/>
 					<Photo src={indianPaintbrush} alt='Indian Paintbrush' />
 					<Photo src={sarcodesSanguinea} alt='Sarcodes sanguinea' />
-					<Photo className='row-span-2' src={tronc} alt="Tronc d'arbre" />
+					<Photo
+						className='row-span-2'
+						src={tronc}
+						alt="Tronc d'arbre"
+					/>
 				</PhotosContainer>
 				<h1>Joshua Tree</h1>
 				<p>
-					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-					Alias pariatur consequuntur adipisci repellat quisquam
-					aliquam reiciendis. Debitis quisquam excepturi, maxime
-					voluptate corrupti incidunt et maiores architecto officiis
-					tempora dolor odio. Odio temporibus provident culpa earum ab
-					doloremque doloribus nostrum impedit quia aliquid quae,
-					suscipit, accusantium cum modi et voluptas assumenda.
+					Joshua Tree, où le désert se pare de mysticisme, séduit par
+					ses formations rocheuses étranges et ses arbres iconiques
+					qui semblent tout droit sortis d&apos;un rêve.
 				</p>
+				<PhotosContainer className='six-pictures'>
+					<Photo src={jt1} alt='Joshua Tree' />
+					<Photo src={jt5} alt='Joshua Tree' className='row-span-2' />
+					<Photo src={jt2} alt='Joshua Tree' />
+					<Photo src={jt3} alt='Joshua Tree' />
+					<Photo src={jt4} alt='Joshua Tree' />
+					<Photo src={jt26} alt='Joshua Tree' className='row-span-2' />
+				</PhotosContainer>
+				<PhotosContainer className='six-pictures'>
+					<Photo src={jt6} alt='Joshua Tree' />
+					<Photo src={jt10} alt='Joshua Tree' className='row-span-2' />
+					<Photo src={jt7} alt='Joshua Tree' />
+					<Photo src={jt8} alt='Joshua Tree' />
+					<Photo src={jt9} alt='Joshua Tree' />
+					<Photo src={jt13} alt='Joshua Tree'  className='row-span-2' />
+				</PhotosContainer>
+				<PhotosContainer className='four-pictures'>
+					<Photo src={jt12} alt='Joshua Tree' />
+					<Photo src={jt14} alt='Joshua Tree' />
+					<Photo src={jt11} alt='Joshua Tree' />
+					<Photo src={jt15} alt='Joshua Tree' />
+				</PhotosContainer>
+				<PhotosContainer className='four-pictures'>
+					<Photo src={jt16} alt='Joshua Tree' />
+					<Photo src={jt17} alt='Joshua Tree' />
+					<Photo src={jt19} alt='Joshua Tree' />
+					<Photo src={jt18} alt='Joshua Tree' />
+				</PhotosContainer>
+				<PhotosContainer className='five-pictures'>
+					<Photo src={jt24} alt='Joshua Tree' />
+					<Photo src={jt25} alt='Joshua Tree' />
+					<Photo src={jt29} alt='Joshua Tree' />
+					<Photo src={jt28} alt='Joshua Tree' />
+					<Photo src={jt27} alt='Joshua Tree' />
+				</PhotosContainer>
+				<PhotosContainer className='four-pictures'>
+					<Photo src={jt20} alt='Joshua Tree' />
+					<Photo src={jt21} alt='Joshua Tree' />
+					<Photo src={jt22} alt='Joshua Tree' />
+					<Photo src={jt23} alt='Joshua Tree' />
+				</PhotosContainer>
 				<h1>Direction Williams</h1>
 				<p>
 					Lorem ipsum, dolor sit amet consectetur adipisicing elit.
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,6 @@
-import Card from "../components/Card";
-import { SectionContainer, CardContainer  } from "../styles/index.css";
-import { MainContainer, TextWrapper } from "../styles/app.css";
+import Card from '../components/Card';
+import { SectionContainer, CardContainer } from '../styles/index.css';
+import { MainContainer, TextWrapper } from '../styles/app.css';
 import hero from '../assets/images/index/hero.jpg';
 import burbank from '../assets/images/burbank/hero.jpg';
 import palmSprings from '../assets/images/palm-springs/hero.jpg';
@@ -13,39 +13,95 @@ import sequoiaPark from '../assets/images/sequoia-park/hero.jpg';
 import yosemite from '../assets/images/yosemite/hero.jpg';
 import sanFrancisco from '../assets/images/san-francisco/hero.jpg';
 
-export default function Index() {   
-    return (
-        <>
-            <MainContainer src={hero}>
-                <TextWrapper className="woody">
-                    <p>Cette terre est la tienne,</p>
-                    <p>Cette terre est la mienne</p>
-                    <p>Du Pacifique à l&apos;île de Manhattan</p>
-                    <p>Des forêts de séquoias,</p>
-                    <p>Aux eaux du Gulf Stream,</p>
-                    <p>Cette terre faite pour toi,</p>
-                    <p>faite pour moi.</p>
-                    <span>Woody Guthrie</span>
-                </TextWrapper>
-            </MainContainer>
-            
-            <SectionContainer>
-                <h1>Découvrons ensemble notre voyage !</h1>
-                <p>Je suis heureux de vous accueillir pour vous faire partager notre fabuleux périple aux USA.</p>
-                <p>Ci-dessous les différentes étapes qui ont jallonnées notre parcours.</p>
-                <CardContainer>
-                    <Card to='burbank' src={burbank} title="Los Angeles" desc="C'est ici que tout commence, le 21 mai 2022 de Paris vers Los Angeles."/>
-                    <Card to='/palm-springs' src={palmSprings} title="Palm Springs" desc="Un oasis dans le désert. Vue imprenable depuis Mount San Jacinto et Joshua Tree."/>
-                    <Card to='/grand-canyon' src={grandCanyon} title="Le Grand Canyon" desc="Une merveille géologique."/>
-                    <Card to='/page' src={page} title="Page" desc="Un lieu absolument magique. Visite de Antelope Canypon et Horseshoe Bend."/>
-                    <Card to='/monument-valley' src={monumentValley} title="Monument Valley" desc="La vallée des rocs."/>
-                    <Card to='/kanab' src={kanab} title="Kanab" desc="Un paysage unique au monde. Découverte de Bryce Canyon et Zion Park."/>
-                    <Card to='/death-valley' src={deathValley} title="Death Valley" desc="Des salines aux dunes de sable et montagnes."/>
-                    <Card to='/sequoia-park' src={sequoiaPark} title="Sequoia Park" desc="Une expérience époustouflante."/>
-                    <Card to='/yosemite' src={yosemite} title="Yosemite" desc="Un joyau de la nature."/>
-                    <Card to='/san-francisco' src={sanFrancisco} title="San Francisco" desc="Berceau de l'innovation et de la diversité."/>
-                </CardContainer>
-            </SectionContainer>
-        </>
-    )
+export default function Index() {
+	return (
+		<>
+			<MainContainer src={hero}>
+				<TextWrapper className='woody'>
+					<p>Cette terre est la tienne,</p>
+					<p>Cette terre est la mienne</p>
+					<p>Du Pacifique à l&apos;île de Manhattan</p>
+					<p>Des forêts de séquoias,</p>
+					<p>Aux eaux du Gulf Stream,</p>
+					<p>Cette terre faite pour toi,</p>
+					<p>faite pour moi.</p>
+					<span>Woody Guthrie</span>
+				</TextWrapper>
+			</MainContainer>
+
+			<SectionContainer>
+				<h1>Découvrons ensemble notre voyage !</h1>
+				<p>
+					Je suis heureux de vous accueillir pour vous faire partager
+					notre fabuleux périple aux USA.
+				</p>
+				<p>
+					Ci-dessous les différentes étapes qui ont jallonnées notre
+					parcours.
+				</p>
+				<CardContainer>
+					<Card
+						to='burbank'
+						src={burbank}
+						title='Los Angeles'
+						desc="C'est ici que tout commence, le 21 mai 2022 de Paris vers Los Angeles."
+					/>
+					<Card
+						to='/palm-springs'
+						src={palmSprings}
+						title='Palm Springs'
+						desc='Un oasis dans le désert. Vue imprenable depuis Mount San Jacinto et Joshua Tree.'
+					/>
+					<Card
+						to='/grand-canyon'
+						src={grandCanyon}
+						title='Le Grand Canyon'
+						desc='Une merveille géologique.'
+					/>
+					<Card
+						to='/page'
+						src={page}
+						title='Page'
+						desc='Un lieu absolument magique. Visite de Antelope Canypon et Horseshoe Bend.'
+					/>
+					<Card
+						to='/monument-valley'
+						src={monumentValley}
+						title='Monument Valley'
+						desc='La vallée des rocs.'
+					/>
+					<Card
+						to='/kanab'
+						src={kanab}
+						title='Kanab'
+						desc='Un paysage unique au monde. Découverte de Bryce Canyon et Zion Park.'
+					/>
+					<Card
+						to='/death-valley'
+						src={deathValley}
+						title='Death Valley'
+						desc='Des salines aux dunes de sable et montagnes.'
+					/>
+					<Card
+						to='/sequoia-park'
+						src={sequoiaPark}
+						title='Sequoia Park'
+						desc='Une expérience époustouflante.'
+					/>
+					<Card
+						to='/yosemite'
+						src={yosemite}
+						title='Yosemite'
+						desc='Un joyau de la nature.'
+					/>
+					<Card
+						to='/san-francisco'
+						src={sanFrancisco}
+						title='San Francisco'
+						desc="Berceau de l'innovation et de la diversité."
+					/>
+				</CardContainer>
+			</SectionContainer>
+		</>
+	);
 }
